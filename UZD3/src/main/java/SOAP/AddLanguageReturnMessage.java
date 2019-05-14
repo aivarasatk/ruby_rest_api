0e6ba981e675e42805898b35da2a0aa5a4db78a8
aivarasatk <42089270+aivarasatk@users.noreply.github.com>
2019-05-14 22:19:52 +0300

    ??

--- UZD3/src/main/java/SOAP/AddLanguageReturnMessage.java
+++ UZD3/src/main/java/SOAP/AddLanguageReturnMessage.java
@@ -9,17 +9,11 @@ public class AddLanguageReturnMessage {
         this.id = id;
     }
 
-    public String getMessage() {
-        return message;
-    }
 
     public void setMessage(String message) {
         this.message = message;
     }
 
-    public Integer[] getId() {
-        return id;
-    }
 
     public void setId(Integer[] id) {
         this.id = id;
