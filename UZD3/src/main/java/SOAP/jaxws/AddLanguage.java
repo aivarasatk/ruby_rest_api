4c8c08242c3d69284defbcb32ce3dc3f1c9a0d79
aivarasatk <42089270+aivarasatk@users.noreply.github.com>
2019-05-14 22:33:17 +0300

    added more fields add lang

--- UZD3/src/main/java/SOAP/jaxws/AddLanguage.java
+++ UZD3/src/main/java/SOAP/jaxws/AddLanguage.java
@@ -22,6 +22,47 @@ public class AddLanguage {
     @XmlElement(required = true, name = "strongly_typed", namespace = "")
     private boolean strongly_typed;
 
+    @XmlElement(name = "reserved_words", namespace = "")
+    private String[] reserved_words;
+    @XmlElement(name = "popular_editor", namespace = "")
+    private String popular_editor;
+    @XmlElement(name = "release_year", namespace = "")
+    private int release_year;
+    @XmlElement(name = "creator", namespace = "")
+    private String creator;
+
+    public String[] getReserved_words() {
+        return reserved_words;
+    }
+
+    public void setReserved_words(String[] reserved_words) {
+        this.reserved_words = reserved_words;
+    }
+
+    public String getPopular_editor() {
+        return popular_editor;
+    }
+
+    public void setPopular_editor(String popular_editor) {
+        this.popular_editor = popular_editor;
+    }
+
+    public int getRelease_year() {
+        return release_year;
+    }
+
+    public void setRelease_year(int release_year) {
+        this.release_year = release_year;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
     public String getLanguage() {
         return language;
     }
